0e154b38e13a77556f1de088d7a0f1c4c02869f0
Evelyn Kumeh <evelynkumeh@Evelyns-Air.lan>
2025-04-30 18:00:42 -0500

    Created first react project

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import cutebabyone from './assets/cutebabytwo.jpeg'
+import cutebabytwo from '/cutebabyone.jpeg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        count is {count}
+        <a href="public/cutebabyone.jpeg" target="_blank">
+          <img src={cutebabyone} className="logo" alt="Vite logo" />
+        </a>
+        <a href="src/assets/cutebabytwo.jpeg" target="_blank">
+          <img src={cutebabytwo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
